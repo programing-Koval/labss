6527a02e66cfd2bf989bb017d971a8e010272a1c
vlad koval <115104050+programing-Koval@users.noreply.github.com>
2022-12-16 02:59:00 +0200

    Update main.c

--- labs/lab 04/src/main.c
+++ labs/lab 04/src/main.c
@@ -7,7 +7,7 @@ float znv = v;
 #define PI 3.14f
 float r1  = 2 * (float) PI * (float) radius;
 float r2  = PI  * (float) radius * (float) radius;
-float r3  = 1.33 * (float) PI * (float) radius * (float) radius * (float) radi>
+float r3  = 1.33 * (float) PI * (float) radius * (float) radius * (float) radius;
 if(znv == l){
  znv = r1;
 }else if(znv == s){
